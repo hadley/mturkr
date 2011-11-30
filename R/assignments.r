79cc5bd47833bf18442543675e4ce99d6706fe6c
hadley <h.wickham@gmail.com>
2011-11-30 11:36:08 -0600

    Documentation & formatting improvements

--- R/assignments.r
+++ R/assignments.r
@@ -1,3 +1,19 @@
+#' Get all assignments for a task.
+#'
+#' Most of the time you should not need to call this directly - instead use
+#' \code{\link{review_task}}.
+#'
+#' @inheritParams publish_task
+#' @param status Which status of assignments to retrieved: "approved",
+#'   "rejected", or "submitted" (i.e. assignments waiting approval).
+#' @return A data frame with a column for each question (named according to
+#'   the question identifier. Multiple anwsers (e.g. from multiple selection
+#'   questions) are separated with the unicode record separater, 
+#'   \code{"\u{001F}"}. Additional columns provided by MTurk data are
+#'   assigmnent_id, worker_id, auto_approval_time, accept_time and
+#'   submit_time.
+#' @export
+#' @importFrom plyr ldply
 get_assignments <- function(task = NULL, status, ...) {
   task <- as.task(task)
   template <- load_template(task)
@@ -7,17 +23,18 @@ get_assignments <- function(task = NULL, status, ...) {
   ldply(ids, get_assignments_for_hit, task = task, status = status, ...)
 }
 
-
-
+#' @importFrom plyr ldply
 get_assignments_for_hit <- function(hit_id, task, status, ...) {
-  status <- match.arg(status, c("approved", "rejected", "submitted", "all"))
+  status <- match.arg(status, c("approved", "rejected", "submitted"))
   
+  # Find out how many pages of results there are
   xml <- mturk_task_req(task, "GetAssignmentsForHIT", 
     HITId = hit_id, PageSize = 1, PageNumber = 1, ...)
   total <- as.numeric(xmlValue(
     getNodeSet(xml, "//TotalNumResults")[[1]][[1]]))
   pages <- ceiling(total / 100)
 
+  # Load all results into a single data frame
   get_ids <- function(page) {
     xml <- mturk_task_req(task, "GetAssignmentsForHIT", 
       HITId = hit_id, PageSize = 100, PageNumber = page, ...)
@@ -27,7 +44,7 @@ get_assignments_for_hit <- function(hit_id, task, status, ...) {
   ldply(seq_len(pages), get_ids)
 }
 
-
+#' @importFrom plyr ldply
 extract_answers <- function(assignment) {
   assignment_id <- xmlValue(assignment[["AssignmentId"]])
   worker_id <- xmlValue(assignment[["WorkerId"]])
@@ -45,8 +62,9 @@ extract_answers <- function(assignment) {
   question_ids <- vapply(answers, function(x) xmlValue(x[["QuestionIdentifier"]]), character(1))
   
   collapse_answers <- function(xml) {
-    a <- getNodeSet(xml, "//FreeText|//SelectionIdentifier|//OtherSelectionText")
-    str_c(vapply(a, xmlValue, character(1)), collapse = "\u{001F}")
+    answer_nodes <- "//FreeText|//SelectionIdentifier|//OtherSelectionText"
+    a_nodes <- getNodeSet(xml, answer_nodes)
+    str_c(vapply(a_nodes, xmlValue, character(1)), collapse = "\u{001F}")
   }
   answers <- lapply(answers, collapse_answers)
   names(answers) <- question_ids
